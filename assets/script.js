68c8c2972fe7e62314801d3163a95bac9c869f9f
Titus Knox <knoxtitus@gmail.com>
2022-01-15 12:45:05 -0500

    added more code

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,18 @@
+// Assign elements to global variables
+let searchBtn = $("#searchBtn");
+let textBox = $("#textBox");
+let currentDay = $("#currentForcast");
+let futureForcast = $("#futureForcast");
+let searchHistory = $("#searchHistory");
 
+
+
+$(document).ready(function(){
+
+// Add click event for city search
+searchBtn.click = function() {
+    let input = textBox.val();
+    console.log("worked");
+};
+
+})
