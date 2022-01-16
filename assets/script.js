58654a10e8a027a8667292d3da6c0f6e0dd57b78
Titus Knox <knoxtitus@gmail.com>
2022-01-15 21:39:25 -0500

    all fetch requests work

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,46 @@
 
 $(document).ready(function(){
-// Assign global variables 
+console.log("ready");
+    // Assign global variables 
 let searchBtn = $("#searchBtn");
 let searchBox = $(".SearchBox")
 let textBox = $("#textBox");
 let currentDay = $("#currentForcast");
 let futureForcast = $("#futureForcast");
 let searchHistory = $("#searchHistory");
-console.log("ready")
-console.log(textBox.val())
-// Add click event for city search
+
+
+// GIVEN a weather dashboard with form inputs
+// WHEN I search for a city
+// THEN I am presented with current and future conditions for that city and that city is added to the search history
 searchBtn.click(function() {
     let input = textBox.val();
+    let website = "http://api.openweathermap.org/data/2.5/weather?q=" + input + "&units=imperial&appid=302ad37fa1a08f4e45663c5649a34fb1";
+    
+    if(input){
+        console.log("fetch start");
+        fetch(website)
+        .then(response => response.json())
+        // .then(results => console.log(results))
+        .then(function(result){
+            let temprature = result.main.temp;
+            let cityName = result.name;
+            let date = moment().format("MMMM Do YYYY");
+            let logo = result.weather.icon;
+            let humidity = result.main.humidity;
+            let windSpeed = result.wind.speed;
+            let latitude = result.coord.lat;
+            let longitude = result.coord.lon;
+        
+           fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&exclude=minutely,hourly,daily,alerts&appid=302ad37fa1a08f4e45663c5649a34fb1")
+           .then(response => response.json())
+           .then(function(fetch2){
+            let uvIndex = fetch2.current.uvi;
+           })
+
+           
+        })
+    }
 
 })
 
